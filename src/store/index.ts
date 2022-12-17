2e683d7102f5fd02b848f35e3ad368c2cd4e9439
xya499 <302870488@qq.com>
2022-12-17 16:54:09 +0800

    init

---
+++ src/store/index.ts
@@ -0,0 +1,13 @@
+import { defineStore } from "pinia";
+import { Names } from './name'
+
+export const useMainStore = defineStore(Names.Main, {
+  state: () => {
+    return {
+
+    }
+  },
+  actions: {},
+  getters: {}
+})
+
---
+++ src/views/home/home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>home</div>
+  <el-button type="primary">Primary</el-button>
+</template>
+
+<script lang='ts' setup>
+
+</script>
+
+<style lang='less' scoped>
+
+</style>
